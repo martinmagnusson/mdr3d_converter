7d4018d69b008d5b811fc4b46174f6db81a4c699
Fredrik Persson <frepeh173@studentmail.oru.se>
2022-01-04 15:12:07 +0100

    small update to format.

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-# shared Variables
+# shared Variables in program
 logging_enabled = False
 Filetypes = ["ROS Gridmap", "2D Standard", "Pointcloud", "Test XSD"]
 Font = "Helvetica, 14"
 Filetype = Filetypes[0] ##Default filetype set
+
+# Related to file conversion, reset after each conversion.
 program_path = ""
 Selected_file_path = ""
 Selected_file_path_dir = ""
