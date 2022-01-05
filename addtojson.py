96570fab6588fd1065db0f4e66b20217578b8854
Fredrik Persson <frepeh173@studentmail.oru.se>
2022-01-05 09:53:57 +0100

    Simplified some addtojson code.

--- addtojson.py
+++ addtojson.py
@@ -10,10 +10,10 @@ import time
 import re
 
 
-def Rosgrid(properties, filename):
+def Rosgrid():
     # Currently everything is added as its own "Values" key, not sure if correct.
-    with open(filename, 'r+') as file:
-        log.logprint("Opened file:" + str(filename))
+    with open(config.output_name, 'r+') as file:
+        log.logprint("Opened file:" + str(config.output_name))
         # log.logprint(properties)
         file_data = json.load(file)
         # file_data = json.load(file)  # First we load existing data into a dict.
@@ -31,26 +31,26 @@ def Rosgrid(properties, filename):
         file_data["properties"].update(
             {"coordinate_system": "relative"})  # temp
         ##Assumption! z resolution is same as x and y, Maybe better set z as 1?
-        res_array = [float(properties["resolution"]),float(properties["resolution"]),float(properties["resolution"])]
-        log.logprint("Resolution taken from x,y as " + str(float(properties["resolution"])) + "and assumed same for z.")
+        res_array = [float(config.properties["resolution"]),float(config.properties["resolution"]),float(config.properties["resolution"])]
+        log.logprint("Resolution taken from x,y as " + str(float(config.properties["resolution"])) + "and assumed same for z.")
         file_data["properties"].update(
             {"resolution": res_array})
         file_data["properties"].update(
-            {"size": [int(properties["width"]), int(properties["height"]), int(1)]})
+            {"size": [int(config.properties["width"]), int(config.properties["height"]), int(1)]})
         file_data["properties"].update(
-            {"localmap_id": filename})
+            {"localmap_id": config.filename})
         file_data["properties"].update(
-            {"list_of_characteristics": {"C_name": "ocupancy"}})
+            {"list_of_characteristics": {"C_name": "occupancy"}})
         file_data["properties"].update( 
-            {"list_of_voxels": properties["pixels"]})
+            {"list_of_voxels": config.properties["pixels"]})
         file.seek(0)  # Sets file's current position at offset.
         json.dump(file_data, file, indent=4)  # convert back to json.
 
 
-def Pointcloud(properties, filename):
+def Pointcloud():
     # Currently everything is added as its own "Values" key, not sure if correct.
-    with open(filename, 'r+') as file:
-        log.logprint("Opened file:" + str(filename))
+    with open(config.output_name, 'r+') as file:
+        log.logprint("Opened file:" + str(config.output_name))
         # log.logprint(properties)
         file_data = json.load(file)
         # file_data = json.load(file)  # First we load existing data into a dict.
@@ -68,8 +68,8 @@ def Pointcloud(properties, filename):
         file_data["properties"].update(
             {"coordinate_system": "relative"})  # temp
         file_data["properties"].update(
-            {"list_of_points": properties["list_of_points"]})
+            {"list_of_points": config.properties["list_of_points"]})
         # <-- get real name not path..
-        file_data["properties"].update({"localmap_id": filename})
+        file_data["properties"].update({"localmap_id": config.output_name})
         file.seek(0)  # Sets file's current position at offset.
         json.dump(file_data, file, indent=4)  # convert back to json.
